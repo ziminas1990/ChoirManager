cfa9400f58c16d6f33151a93de9b4fdda976f34b
Aleksandr Zimin <aleksandr.s.zimin@jettycloud.com>
2025-04-16 17:18:11 +0400

    GH-7 Drive-by: fix feedbacks storage issue

--- src/adapters/feedback_storage/google_spreadsheet.ts
+++ src/adapters/feedback_storage/google_spreadsheet.ts
@@ -41,41 +41,41 @@ function try_parse_header(header: string[]): StatusWith<TableColumns> {
     return StatusWith.ok().with(info as TableColumns);
 }
 
-function get_voice(voice: string): Voice {
+function get_voice(voice: string): Voice | undefined {
     switch (voice.toLowerCase()) {
         case "alto": return Voice.Alto;
         case "soprano": return Voice.Soprano;
         case "tenor": return Voice.Tenor;
         case "baritone": return Voice.Baritone;
-        default: return Voice.Unknown;
+        default: return undefined;
     }
 }
 
 function try_parse_row(row: string[], columns: TableColumns): StatusWith<Feedback> {
-    const tgid = row[columns.tgid];
-    if (!tgid || tgid.length === 0) {
-        return StatusWith.fail("No 'tgid' column found");
-    }
 
     const timestamp = row[columns.timestamp];
     if (!timestamp || timestamp.length === 0) {
         return StatusWith.fail("No 'timestamp' column found");
     }
 
+    const tgid = row[columns.tgid] || undefined;
+    const name_surname = row[columns.who] || undefined;
+
     const date = new Date(timestamp);
-    const who = row[columns.who];
+    const who = (tgid) ? {
+        tgid: tgid,
+        name_surname: name_surname || "unknown"
+    } : undefined;
     const voice = row[columns.voice];
     const details = row[columns.details];
 
     const feedback: Feedback = {
         date,
         details,
-        who: {
-            tgid: who,
-            name_surname: who
-        },
+        who,
         voice: get_voice(voice)
     };
+
     return StatusWith.ok().with(feedback);
 }
 
@@ -136,9 +136,12 @@ export class GoogleSpreadsheetFeedbackStorage implements IFeedbackStorage {
 
         this.feedbacks = [];
         let invalid_rows = 0;
+        let row_number = 0;
         for (const row of table.slice(1)) {
+            row_number++;
             const feedback_status = try_parse_row(row, columns.value!);
             if (!feedback_status.ok()) {
+                this.journal.log().error(`Invalid row #${row_number}: ${feedback_status.what()}`);
                 invalid_rows++;
                 continue;
             }
