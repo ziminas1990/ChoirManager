4d91925f54fa42e01a09962461f0d7b803c832c1
Aleksandr Zimin <aleksandr.s.zimin@jettycloud.com>
2025-07-01 19:48:13 +0400

    GH-14 Remove redundant dependency

--- src/api/google_auth.ts
+++ src/api/google_auth.ts
@@ -1,9 +1,6 @@
 import fs from "fs";
 import { google, Auth, sheets_v4, docs_v1 } from "googleapis";
 
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
 import { Status } from "@src/status.js";
 import { Firestore } from "@google-cloud/firestore";
 
@@ -16,7 +13,7 @@ const scopes = [
 
 export class GoogleAuth {
 
-    //private static google_cloud_key_file: string;
+    private static google_cloud_key_file: string;
     private static auth?: Auth.GoogleAuth;
     private static sheets?: sheets_v4.Sheets;
     private static documents?: docs_v1.Docs;
@@ -24,7 +21,7 @@ export class GoogleAuth {
 
     public static async authenticate(google_cloud_key_file: string): Promise<Status> {
 
-        //GoogleAuth.google_cloud_key_file = google_cloud_key_file;
+        GoogleAuth.google_cloud_key_file = google_cloud_key_file;
 
         let credentials: any | undefined = undefined;
         try {
@@ -40,8 +37,6 @@ export class GoogleAuth {
             credentials: credentials,
             scopes: scopes,
         });
-
-        initializeApp({ credential: cert(credentials) });
 
         return Status.ok();
     }
@@ -62,7 +57,11 @@ export class GoogleAuth {
 
     public static get_firestore(database_id: string): Firestore {
         if (!GoogleAuth.firestore.has(database_id)) {
-            GoogleAuth.firestore.set(database_id, getFirestore(database_id));
+            const instance = new Firestore({
+                databaseId: database_id,
+                keyFilename: GoogleAuth.google_cloud_key_file,
+            });
+            GoogleAuth.firestore.set(database_id, instance);
         }
         return GoogleAuth.firestore.get(database_id)!;
     }
