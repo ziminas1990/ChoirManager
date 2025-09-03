bb06fae893f7439900463d25bd6a0bbd5a0ec68f
IDbok <bokarev.fic@gmail.com>
2025-09-03 19:14:21 +0400

    hotfix: update transaction type for membership transaction store

--- src/use_cases/deposit_actions.ts
+++ src/use_cases/deposit_actions.ts
@@ -176,14 +176,30 @@ export class DepositActions {
             }
         }
 
-        await Runtime.get_instance()
-        .get_transactions_storage()
-        ?.add_transaction({
-            date: new Date(),
-            change: changes.total_change,
-            balance_after: deposit.balance,
-            tgid: user.data.tgid
-        });
+        if (changes.balance) {
+            await Runtime.get_instance()
+            .get_transactions_storage()
+            ?.add_transaction({
+                date: new Date(),
+                tgid: user.data.tgid,
+                type: "balance",
+                before: changes.balance![0],
+                after: changes.balance![1],
+            });
+        }
+
+        if(changes.membership){
+            await Runtime.get_instance()
+            .get_transactions_storage()
+            ?.add_transaction({
+                date: new Date(),
+                tgid: user.data.tgid,
+                type: "membership",
+                membership_month: changes.membership![0][0],
+                before: changes.membership![0][1],
+                after: changes.membership![0][2],
+            });
+        }
         
         return Status.ok();
     }
