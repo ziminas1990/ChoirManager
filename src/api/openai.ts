c92c4556a078893d93d54b3c0908305bbf47764f
Aleksandr Zimin <ziminas1990@gmail.com>
2026-05-29 06:16:15 +0000

    Revert using of high priority for OpenAI API

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -185,8 +185,7 @@ export class OpenaiLLM implements ILLM {
                 messages,
                 response_format: get_response_format(response_format),
                 tools: openai_tools.length > 0 ? openai_tools : undefined,
-                tool_choice: openai_tools.length > 0 ? tools?.choice ?? "auto" : undefined,
-                service_tier: "priority",
+                tool_choice: openai_tools.length > 0 ? tools?.choice ?? "auto" : undefined
             });
 
             if (completion.choices.length === 0) {
